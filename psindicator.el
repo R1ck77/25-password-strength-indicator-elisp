0020477fa448c5c0dfc4bc6ea10c90b3117fad7a
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-11-17 19:40:09 +0100

    Add the backbone of the strength to string conversion

---
+++ psindicator.el
@@ -0,0 +1,13 @@
+
+
+(defconst psindicator--strength-to-assessment ["very weak" "weak" "stong" "very strong"])
+
+(defun psindicator-password-validator (password)
+  )
+
+(defun psindicator--string-result (password-strength)
+  (message "The password '%s' is %s" )
+  )
+
+
+(provide 'psindicator)
