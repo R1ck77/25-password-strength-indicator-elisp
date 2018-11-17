a049aec44ebf1fc56018aae1286183ec1ab53cac
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-11-17 21:57:47 +0100

    Add password predicates

--- psindicator.el
+++ psindicator.el
@@ -2,6 +2,25 @@
 
 (defconst psindicator--strength-to-assessment ["very weak" "weak" "stong" "very strong"])
 
+(defun psindicator--very-weak-password? (password)
+  (and (< (length password) 8)
+       (string-match "^[0-9]*$" password)))
+
+(defun psindicator--weak-password? (password)
+  (and (< (length password) 8)
+       (string-match "^[a-zA-Z]*$" password)))
+
+(defun psindicator--strong-password? (password)
+  (and (>= (length password) 8)
+       (string-match "[a-zA-Z]." password)
+       (string-match "[0-9]." password)))
+
+(defun psindicator--very-strong-password? (password)
+  (and (>= (length password) 8)
+       (string-match "[a-zA-Z]." password)
+       (string-match "[0-9]." password)
+       (string-match "[^0-9a-zA-Z]." password)))
+
 (defun psindicator-password-validator (password)
   )
 
